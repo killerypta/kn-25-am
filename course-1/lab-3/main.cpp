9ea08039f031f048ad5fbd8e5724b6b9f0a17240
killerypta <killerypta@proton.me>
2025-12-21 22:39:11 +0200

    Added several updates

--- course-1/lab-3/main.cpp
+++ course-1/lab-3/main.cpp
@@ -3,57 +3,67 @@
 
 using namespace std;
 
-double task1(double x, double y, double z);
+double firstTask(double x, double y, double z);
+double secondTask();
+void callTask(int n);
+
+/*
+ [M] - Menu
+ [I] - Input
+ [S] - Succesfull
+ [E] - Error
+ */
 
 int main() {
   int n;
 
-  double x, y, z, result;
-
-  cout << "Enter random number: ";
+  cout << "[M] Enter number (< 8 || >= 8): ";
   cin >> n;
 
-  if (n % 15 < 8) {
-    cout << "[task 1]\n"
-         << "Enter value for x, y, z: ";
-    cin >> x >> y >> z;
-
-    result = task1(x, y, z);
-
-    cout << "\nFormula: 3 + exp(y - 1) / 1 + pow(x, 2) * fabs(y - tan(z))\n"
-         << "Result: " << result << endl;
-  } else {
-    int x, s;
-    s = 0;
-
-    cout << "[task 2]\n"
-         << "Enter real numbers (multiple 2; positive ends calculation): "
-         << endl;
-
-    do {
-      cin >> x;
-      s += x;
-      cout << "Sum: " << s << endl;
-    } while (x < 0);
-  }
+  callTask(n);
 
   return 0;
 }
 
-double task1(double x, double y, double z) {
+double firstTask(double x, double y, double z) {
   return 3 + exp(y - 1) / 1 + pow(x, 2) * fabs(y - tan(z));
 }
 
-int task2(int x, int s) {
-  do {
+double secondTask() {
+  int x = 0;
+  int s = 0;
+  int i = 0;
+
+  for (x = 0; x <= 0; ++i) {
     cin >> x;
-    if (x % 2) {
-      s += x;
-      cout << "Current sum: " << s << endl;
+
+    if (x > 0) {
+      cout << "\n[S] Result: " << s << endl;
     } else {
-      cout << "Number dose'nt multple 2!" << endl;
-      break;
+      if (i % 2 == 0) {
+        s += x;
+      }
     }
-  } while (x < 0);
+  }
+
   return s;
+}
+
+void callTask(int n) {
+  double x, y, z;
+
+  if (n % 15 < 8) {
+    cout << "\n[S] Executing first task." << endl;
+
+    cout << "\n[I] Enter value for x, y, z: ";
+    cin >> x >> y >> z;
+
+    cout << "\n[S] Result: " << firstTask(x, y, z) << endl;
+  } else {
+    cout << "\n[S] Executing second task." << endl;
+
+    cout << "\nEnter real numbers (positive - stop): ";
+
+    secondTask();
+  }
 }
--- course-1/lab-4/main.cpp
+++ course-1/lab-4/main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 
-using namespace std;
-
 #define N 30
+
+using namespace std;
 
 void getMax(int a[], int size);
 
 int main() {
-  int a[N] = {0,   325, 6,   290, 313, 524, 168, 693, 178, 656,
-              886, 946, 575, 702, 460, 669, 864, 99,  468, 493,
-              657, 488, 412, 750, 195, 602, 658, 287, 7,   739};
+  int a[N] = {27, 99, 79, 75, 31, 94, 89, 35, 99, 79, 12, 25, 48, 82, 53,
+              22, 58, 64, 90, 60, 66, 85, 31, 9,  40, 89, 36, 37, 98, 40};
 
   getMax(a, 30);
+
   return 0;
 }
 
@@ -24,5 +24,33 @@ void getMax(int a[], int size) {
     }
     cout << max << endl;
   }
-  cout << max << endl;
 }
+
+// #include <iostream>
+//
+// using namespace std;
+//
+// #define N 30
+//
+// void getMax(int a[], int size);
+//
+// int main() {
+//   int a[N] = {0,   325, 6,   290, 313, 524, 168, 693, 178, 656,
+//               886, 946, 575, 702, 460, 669, 864, 99,  468, 493,
+//               657, 488, 412, 750, 195, 602, 658, 287, 7,   739};
+//
+//   getMax(a, 30);
+//   return 0;
+// }
+//
+// void getMax(int a[], int size) {
+//   int max = a[0];
+//
+//   for (int i = 0; i < size; ++i) {
+//     if (a[i] > max) {
+//       max = a[i];
+//     }
+//     cout << max << endl;
+//   }
+//   cout << max << endl;
+// }
